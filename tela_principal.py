60261f0415ac37cef1444353387737b856e077f3
TSjadness <jadsontavarests@hotmail.com>
2023-10-10 11:51:09 -0300

    update layout

--- tela_principal.py
+++ tela_principal.py
@@ -25,8 +25,8 @@ radio_tremores_ou_suor = IntVar()
 radio_dificuldade_de_dormi = IntVar()
 
 janela.title("Fatores de risco para Covid-19")
-largura = 800
-altura = 600
+largura = 1100
+altura = 750
 pos_x = (janela.winfo_screenwidth() // 2) - (largura // 2) 
 pos_y = (janela.winfo_screenheight() // 2) - (altura // 2) 
 janela.geometry(f"{largura}x{altura}+{pos_x}+{pos_y}") 
@@ -36,7 +36,7 @@ def on_canvas_configure(event):
 
 janela.title("Fatores de risco para Covid-19")
 
-frame_principal = Frame(janela, bg="#C0C0C0")
+frame_principal = Frame(janela, bg="#9DBCD4")
 frame_principal.pack(fill="both", expand=True)
 
 canvas = Canvas(frame_principal, bg="#C0C0C0")
@@ -50,13 +50,12 @@ frame_interno = Frame(canvas, bg="#C0C0C0")
 canvas.create_window((0, 0), window=frame_interno, anchor="nw")
 
 titulo1 = Label(frame_interno, 
-                text="Fatores de risco para Covid-19",
-                font=("Roboto", 20),
+                text="DR.MCMISSIL DIAGNOSTICANDO SUA ANSIEDADE",
+                font=("Roboto", 23, "bold"),
+                fg='blue',
                 bg="#C0C0C0",
-                relief="groove",
-                border=1
                 )
-titulo1.grid(column=0, row=0, columnspan=2, pady=(0, 17), padx=20, sticky="w") 
+titulo1.grid(column=0, row=0, columnspan=2, pady=(50, 50), padx=(200, 50), sticky="w") 
 
 perguntas_respostas = [
     ("Você costuma se sentir excessivamente preocupado com coisas cotidianas?", radio_preocupado),
@@ -75,22 +74,22 @@ linha = 1
 largura_maxima_coluna = 800
 
 for pergunta, variavel in perguntas_respostas:
-    label_pergunta = Label(frame_interno, text=pergunta, font=("Roboto", 13), bg="#C0C0C0", anchor="w")
-    label_pergunta.grid(column=coluna, row=linha, padx=20, pady=(10, 0), sticky="w")  # Alinhe à esquerda
+    label_pergunta = Label(frame_interno, text=pergunta, font=("Roboto", 13, "bold"), bg="white", anchor="w")
+    label_pergunta.grid(column=0, row=linha, padx=(20, 5), pady=10, sticky="e")
     
     if pergunta == "Idade:":
-        label_idade = Label(frame_interno, font=("Roboto", 13), bg="#C0C0C0", anchor="w")
+        label_idade = Label(frame_interno, font=("Roboto", 13, "bold"), bg="#C0C0C0", anchor="w")
         label_idade.grid(column=coluna, row=linha+1, padx=20, pady=(0, 5), sticky="w")
         
-        entrada_idade = Entry(frame_interno, textvariable=box_Idade, font=("Roboto", 13))
-        entrada_idade.grid(column=coluna, row=linha+2, padx=20, pady=(0, 10))
+        entrada_idade = Entry(frame_interno, textvariable=box_Idade, font=("Roboto", 13, "bold"))
+        entrada_idade.grid(column=coluna+1, row=linha, padx=20, pady=(0, 10))
     
     else:
-        radio_sim = Radiobutton(frame_interno, text="Sim", variable=variavel, value=1, font=("Roboto", 13), bg="#C0C0C0")
-        radio_sim.grid(column=coluna, row=linha+1, padx=20, pady=(0, 10))
+        radio_sim = Radiobutton(frame_interno, text="Sim", variable=variavel, value=1, font=("Roboto", 13, "bold"), bg="green",fg='white',)
+        radio_sim.grid(column=1, row=linha, padx=(10, 0), pady=10, sticky="w")
         
-        radio_nao = Radiobutton(frame_interno, text="Não", variable=variavel, value=0, font=("Roboto", 13), bg="#C0C0C0")
-        radio_nao.grid(column=coluna + 1, row=linha+1, padx=20, pady=(0, 10))
+        radio_nao = Radiobutton(frame_interno, text="Não", variable=variavel, value=0, font=("Roboto", 13, "bold"), bg="red",fg='white')
+        radio_nao.grid(column=1, row=linha, padx=(80, 0), pady=10, sticky="w")  # Ajuste o padx aqui
     
 
     coluna += 2
@@ -102,24 +101,24 @@ for pergunta, variavel in perguntas_respostas:
 botaoDiagnostico = Button(frame_interno,
                           text='Realizar Diagnóstico',
                           fg='white',
-                          bg='green',
+                          bg='blue',
                           command=diagnosticar,
-                          font=("Roboto", 13),
+                          font=("Roboto", 13, "bold"),
                           relief="ridge",
                           border=2,
                           )
-botaoDiagnostico.grid(column=0, row=linha+4, columnspan=2, pady=20, padx=20)
+botaoDiagnostico.grid(column=0, row=linha, columnspan=3, pady=20, padx=20)
 
 botaoFechar = Button(frame_interno,
                      text='Fechar',
                      fg='white',
                      bg='Red',
                      command=janela.quit,
-                     font=("Roboto", 13),
+                     font=("Roboto", 13, "bold"),
                      relief="ridge",
                      border=2,
                      )
-botaoFechar.grid(column=0, row=linha+4, columnspan=2, sticky="w", padx=20)
+botaoFechar.grid(column=0, row=linha+1, columnspan=3, pady=20,padx=20)
 
 canvas.bind("<Configure>", on_canvas_configure)
 
